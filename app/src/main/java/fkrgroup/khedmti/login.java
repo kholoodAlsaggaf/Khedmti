01d31a16b085e5131f64935b10e9d3986cc3a8e0
kholoodAlsaggaf <kholood.alsaggaf@gmail.com>
2016-02-02 00:13:47 +0400

    kholood

--- app/src/main/java/fkrgroup/khedmti/login.java
+++ app/src/main/java/fkrgroup/khedmti/login.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class login extends AppCompatActivity {
-
+//hi i'm kholood
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
